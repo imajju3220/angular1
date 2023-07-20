49726a62dee505baae3933ff52945508336d2ff5
Aman Kumar Maddeshiya <aman.k.maddeshiya@zenesys.com>
2023-07-20 16:37:37 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app1';
+export class AppComponent implements OnInit{
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  ngOnInit(): void {
+   
+  }
+  onChangeLength(event : Event){   
+    if (event.target) {
+      const target = event.target as HTMLInputElement;
+      const value = target.value;      
+      const parsedValue = parseInt(value);      
+      if (!isNaN(parsedValue)) {
+          console.log('!isNaN', parsedValue);
+            this.length = parsedValue;
+          }        
+    }    
+  }
+
+  
+
+  onChangeUseLetter(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols
+  }
+ 
+
+  onButtonClick(){
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if(this.includeLetters){
+      validChars += letters;
+    }
+    if(this.includeNumbers){
+      validChars += numbers;
+    }
+    if(this.includeSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';    
+
+    for(let i=0; i<this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }    
+    this.password = generatedPassword;
+   
+  }
 }
